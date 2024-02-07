75b0b3e9c47c90db7eb9810f0f1e9c0adfad8969
williamkoelliker <williamkoelliker@WILLIAMKOEL37F3>
2024-02-07 10:30:49 -0700

    Add project files.

---
+++ MyHobby/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyHobby.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MyHobby/wwwroot/js/calculator.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    $('#calculateTotal').click(function () {
+        // Get the value entered by the user for hours needed
+        var hoursNeeded = parseFloat($('#hoursNeeded').val());
+
+        // Validate if the input is a positive number
+        if (isNaN(hoursNeeded) || hoursNeeded <= 0) {
+            alert('Please enter a valid positive number for hours needed.');
+            return;
+        }
+
+        // Get the hourly rate
+        var hourlyRate = parseFloat($('#hourlyRate').val().replace('$', ''));
+
+        // Calculate the total cost
+        var totalCost = hoursNeeded * hourlyRate;
+        console.log(totalCost)
+        // Display the total cost in the output box
+        $('#totalCost').val('$' + totalCost.toFixed(2));
+    });
+});
+
